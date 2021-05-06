af641bb3ebd8b616afc92e238bdd274ea0c2067c
parsonsmatt <parsonsmatt@gmail.com>
2021-05-06 13:22:31 -0600

    do it in Q

--- src/Database/Persist/Discover.hs
+++ src/Database/Persist/Discover.hs
@@ -6,16 +6,24 @@ module Database.Persist.Discover
     ) where
 
 import Database.Persist.Discover.Exe (getFilesRecursive, stripSuffix)
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+import Data.FileEmbed
 
 -- | Returns a list of all files with the @.persistentmodels@ suffix.
+--
+-- > allFiles :: [FilePath]
+-- > allFiles = $$(findPersistentModelFiles "config/models/")
 --
 -- @since 0.1.0.0
 findPersistentModelFiles
     :: FilePath
     -- ^ The root directory to search from.
-    -> IO [FilePath]
+    -> Q (TExp [FilePath])
 findPersistentModelFiles root = do
-    filter isPersistentModelFile <$> getFilesRecursive root
+    projectRoot <- makeRelativeToProject root
+    files <- runIO $ filter isPersistentModelFile <$> getFilesRecursive projectRoot
+    liftTyped files
   where
     isPersistentModelFile filename =
         case stripSuffix ".persistentmodels" filename of
